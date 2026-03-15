3b79a59047ad3dfb551fbf40fb5065724f394a7a
Niraj Shah <nirajshah950@gmail.com>
2026-03-15 21:41:27 +0530

    Todo Table Defination - id todo status

---
+++ src/main/java/com/todo/www/todo/entity/Todo.java
@@ -0,0 +1,27 @@
+package com.todo.www.todo.entity;
+
+import com.todo.www.todo.Status;
+import com.todo.www.todo.dto.TaskDto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false,updatable = false)
+    private String todo;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+}
