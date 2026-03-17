9753fda194e07bc8e93d508ec9b2dc01a27d8d39
Niraj Shah <nirajshah950@gmail.com>
2026-03-17 23:16:27 +0530

    Define the update user profile service function.

--- src/main/java/com/todo/www/todo/user/service/UserService.java
+++ src/main/java/com/todo/www/todo/user/service/UserService.java
@@ -3,8 +3,10 @@ package com.todo.www.todo.user.service;
 import com.todo.www.todo.dto.ResponseDto;
 import com.todo.www.todo.user.dto.CreateUserDto;
 import com.todo.www.todo.user.dto.LoginUserDto;
+import com.todo.www.todo.user.dto.UpdaetUserDto;
 
 public interface UserService {
     ResponseDto signUp(CreateUserDto createUserDto);
     ResponseDto login(LoginUserDto loginUserDto);
+    ResponseDto updateProfile(UpdaetUserDto updaetUserDto, int userId);
 }
